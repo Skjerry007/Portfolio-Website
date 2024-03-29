043447a76bb4b2a337f0072f46a0a1c1b24735e3
SHASHANK AWASTHI <awasthipriyanshu13@gmail.com>
2024-03-29 20:00:56 +0530

    changes
    chanages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,52 +1,39 @@
-import React, { useEffect } from 'react'
-import { AiOutlineCloudDownload } from 'react-icons/ai'
-import '../App.css'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import React, { useEffect } from 'react';
+import { AiOutlineCloudDownload } from 'react-icons/ai';
+import '../App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const About = () => {
     useEffect(() => {
-        AOS.init({ duration: 1000 })
-    })
+        AOS.init({ duration: 1000 });
+    }, []);
 
     return (
         <div name="about" className='w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-auto'>
                 <div className='pb-8'>
-
-                    <h2 className='text-4xl  font-bold inline border-b-4 border-gray-500'>About</h2>
-
+                    <h2 className='text-4xl  font-bold inline border-b-4 border-gray-500'>About Me</h2>
                 </div>
 
-                <p data-aos="slide-up" data-aos-duration="500" className='text-xl mt-5 ease-in fade-in'>
-                🚀 2.5 Years Experience: Full Stack SDET 🚀
+                <ul className='text-xl mt-5 ease-in fade-in'>
+                    <li data-aos="slide-up" data-aos-duration="500">🚀 2.5 Years Experience: Full Stack SDET 🚀</li>
+                    <li data-aos="slide-up" data-aos-duration="500">🔹 Proficient in Selenium for Web Application Testing and Postman for REST API Testing.</li>
+                    <li data-aos="slide-up" data-aos-duration="500">🔹 Expertise in complex projects covering Smoke, Sanity, Regression, and Functional Testing.</li>
+                    <li data-aos="slide-up" data-aos-duration="500">🔹 Specialized in Blockchain Technology for Supply Chain Management using APIs.</li>
+                    <li data-aos="slide-up" data-aos-duration="500">🔹 Skilled in understanding Requirements and Functional Specifications.</li>
+                    <li data-aos="slide-up" data-aos-duration="500">🔹 Creator of Rest Assured and Karate automation frameworks.</li>
+                    <li data-aos="slide-up" data-aos-duration="500">🔹 Tech blogger, writer, and advocate for the importance of SDET roles.</li>
+                    <li data-aos="slide-up" data-aos-duration="500">🔹 Committed to changing perceptions and elevating the value of SDET roles in companies.</li>
+                </ul>
 
-🔹 Proficient in Selenium for Web Application Testing and Postman for REST API Testing.
-🔹 Expertise in complex projects covering Smoke, Sanity, Regression, and Functional Testing.
-🔹 Specialized in Blockchain Technology for Supply Chain Management using APIs.
-🔹 Skilled in understanding Requirements and Functional Specifications.
-🔹 Creator of Rest Assured and Karate automation frameworks.
-🔹 Tech blogger, writer, and advocate for the importance of SDET roles.
-🔹 Committed to changing perceptions and elevating the value of SDET roles in companies.
-
-Beyond Bugs: Empowering SDET Excellence!
-                </p>
-               { /* <br />
-                <p data-aos="slide-up" data-aos-duration="500" className='text-xl mt-5 ease-in fade-in'>
-                </p>
-                {/* <br />
-                <p data-aos="fade-in" data-aos-duration="500" className='text-xl mt-5 ease-in  fade-in'>
-                    I've worked for many projects for my clients as well as my personal projects. I also contributed for the GDSC RKMGEC web page. I have a Instagram page (@codewithbiki) where I create content for those who are just starting with programming and have more that 70k followers. I have learning attitude and growth mindset which helps me to collaborate with people and work for the society.
-                </p> */}
                 <div data-aos="zoom-in" data-aos-duration="500" className='mb-20 fade-in flex mx-auto items-center justify-center hover:scale-105 duration-300 rounded bg-gradient-to-b from-cyan-500 to-blue-500 lg:w-1/5 md:w-1/5 w-2/4 h-10 text-center cursor-pointer mt-10 gap-2'>
-
-
                     <AiOutlineCloudDownload />
                     <a href="https://drive.google.com/file/d/1IN8zpVevJ8Sfo8N0_idzuFDYeJRHCK-P/view?usp=sharing" ShashankAwasthiSDET_Resume>Download Resume</a>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default About
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,74 +1,66 @@
-import React, { useEffect } from 'react'
-import Dp from "../assets/dp.jpeg"
-import { MdOutlineArrowRightAlt } from 'react-icons/md'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { Link } from 'react-scroll'
-import '../App.css'
-import { HiOutlineMail } from 'react-icons/hi'
-import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs'
+import React, { useEffect } from 'react';
+import Dp from "../assets/dp.jpeg";
+import { MdOutlineArrowRightAlt } from 'react-icons/md';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { Link } from 'react-scroll';
+import '../App.css';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs';
 
 const Home = () => {
-    useEffect(()=> {
-        AOS.init({duration: 1000})
-    })
+    useEffect(() => {
+        AOS.init({ duration: 1000 });
+    }, []);
+
     return (
         <div name="home" className='h-auto w-full bg-gradient-to-b from-black via-black to-gray-800'>
-            <div className='max-w-screen-lg mx-auto flex flex-col items-center
-                            justify-center  px-4 md:flex-row text-white'>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center px-4 md:flex-row text-white'>
                 <div className='flex flex-col justify-center'>
                     <div>
                         <img src={Dp} alt="my profile" data-aos="fade-in" data-aos-duration="500" className='mt-20 rounded-2xl w-1/3 md:w-1/3 lg:w-1/5 my-5 hover:scale-105 duration-300 sm:ease-in' />
                     </div>
-                            <h2 data-aos="fade-in" data-aos-duration="1000" className='text-4xl sm:text-6xl font-bold text-white'>Shashank Awasthi | <span data-aos="fade-left" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black'>Software Development Engineer In Test</span></h2>
-                            <p data-aos="fade-in" data-aos-delay="400" data-aos-duration="2000" className='text-gray-400 py-4 max-w-md w-full'>
-I'm currently working as SDET in Nykaa, with expertise in both Backend and Frontend Automation of more than 3+ years of experience. 
-                            </p>
-                            <p data-aos="fade-in" data-aos-delay="800" data-aos-duration="2000" className='text-gray-400 py-4 max-w-md'>
-                                BEST DIPLOMAT DUBAI | TECH BLOGGER (MEDIUM) | BEYOND BUGS(LINKEDIN PAGE)
-                            </p>
+                    <h2 data-aos="fade-in" data-aos-duration="1000" className='text-4xl sm:text-6xl font-bold text-white'>Shashank Awasthi | <span data-aos="fade-left" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black'>Software Development Engineer In Test</span></h2>
+                    <p data-aos="fade-in" data-aos-delay="400" data-aos-duration="2000" className='text-gray-400 py-4 max-w-md w-full'>
+                        I'm currently working as SDET in Nykaa, with expertise in both Backend and Frontend Automation of more than 3+ years of experience.
+                    </p>
+                    <p data-aos="fade-in" data-aos-delay="800" data-aos-duration="2000" className='text-gray-400 py-4 max-w-md'>
+                        BEST DIPLOMAT DUBAI | TECH BLOGGER (MEDIUM) | BEYOND BUGS(LINKEDIN PAGE)
+                    </p>
 
                     <div className='flex flex-row gap-2'>
-                        <Link to="projects" smooth duration={500} data-aos="fade-up" data-aos-delay="1000" data-aos-duration="2000" className='group rounded bg-gradient-to-r from-blue-600 to-cyan-600 w-1/3 h-8 flex items-center justify-center cursor-pointer mt-5 mb-28' >Portfolio
-
+                        <Link to="projects" smooth duration={500} data-aos="fade-up" data-aos-delay="1000" data-aos-duration="2000" className='group rounded bg-gradient-to-r from-blue-600 to-cyan-600 w-1/3 h-8 flex items-center justify-center cursor-pointer mt-5 mb-28' >
+                            Portfolio
                             <span className='px-2 group-hover:rotate-90 duration-300'>
                                 <MdOutlineArrowRightAlt size={25} />
                             </span>
                         </Link>
-                        <a target='_blank' rel="noreferrer"  href="mailto:awasthipriyanshu13@gmail.com" smooth duration={500} data-aos="fade-up" data-aos-delay="1000" data-aos-duration="2500" className='group hover:scale-105 rounded w-auto h-8 p-3 flex items-center bg-gradient-to-r from-indigo-600 via-purple-600 to-orange-600 justify-center cursor-pointer mt-5 mb-28' >
-
+                        <a target='_blank' rel="noreferrer" href="mailto:awasthipriyanshu13@gmail.com" smooth duration={500} data-aos="fade-up" data-aos-delay="1000" data-aos-duration="2500" className='group hover:scale-105 rounded w-auto h-8 p-3 flex items-center bg-gradient-to-r from-indigo-600 via-purple-600 to-orange-600 justify-center cursor-pointer mt-5 mb-28' >
                             <span className='pr-2'>
                                 <HiOutlineMail size={25} />
                             </span>
                             Hire Me
                         </a>
                         <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/shashank-awasthi-0843a8142/" smooth duration={550} data-aos="fade-up" data-aos-delay="2000" data-aos-duration="3000" className='group hover:scale-105 rounded w-auto h-8 px-1 flex items-center bg-none justify-center cursor-pointer mt-5 mb-28' >
-
                             <span >
-                            <BsLinkedin size={25}></BsLinkedin>
+                                <BsLinkedin size={25}></BsLinkedin>
                             </span>
                         </a>
                         <a target='_blank' rel="noreferrer" href="https://github.com/Skjerry007" smooth duration={600} data-aos="fade-up" data-aos-delay="3000" data-aos-duration="3500" className='group hover:scale-105 rounded w-auto h-8 px-1 flex items-center bg-none justify-center cursor-pointer mt-5 mb-28' >
-
                             <span >
-                            <BsGithub size={25}></BsGithub>
+                                <BsGithub size={25}></BsGithub>
                             </span>
                         </a>
                         <a target='_blank' rel="noreferrer" href="https://www.instagram.com/_iamilluminattiii" smooth duration={650} data-aos="fade-up" data-aos-delay="2500" data-aos-duration="2000" className='group hover:scale-105 rounded w-auto h-8 px-1 flex items-center bg-none justify-center cursor-pointer mt-5 mb-28' >
-
                             <span >
-                            <BsInstagram size={25}></BsInstagram>
+                                <BsInstagram size={25}></BsInstagram>
                             </span>
                         </a>
                     </div>
-
                 </div>
-
-
             </div>
-                
         </div>
     )
 }
 
-export default Home
+export default Home;
